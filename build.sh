4d96132b2565b5cd0b5e6debf79894f8def1fbb9
Robert McNally <robert@arciem.com>
2011-04-11 12:41:56 -0700

    Now building for SDK 4.3 and into separate archives for device and simulator.

--- build.sh
+++ build.sh
@@ -21,7 +21,7 @@ cd openssl-1.0.0a
 ./configure BSD-generic32 --openssldir=/tmp/openssl-1.0.0a-armv6 $OPENSSL_CONFIGURE_OPTIONS &> /tmp/openssl-1.0.0a-armv6.log
 perl -i -pe 's|static volatile sig_atomic_t intr_signal|static volatile int intr_signal|' crypto/ui/ui_openssl.c
 perl -i -pe 's|^CC= gcc|CC= /Developer/Platforms/iPhoneOS.platform/Developer/usr/bin/gcc -arch armv6|g' Makefile
-perl -i -pe 's|^CFLAG= (.*)|CFLAG= -isysroot /Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS4.2.sdk $1|g' Makefile
+perl -i -pe 's|^CFLAG= (.*)|CFLAG= -isysroot /Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS4.3.sdk $1|g' Makefile
 make &> /tmp/openssl-1.0.0a-armv6.log
 make install &> /tmp/openssl-1.0.0a-armv6.log
 popd
@@ -36,7 +36,7 @@ cd openssl-1.0.0a
 ./configure BSD-generic32 --openssldir=/tmp/openssl-1.0.0a-armv7 $OPENSSL_CONFIGURE_OPTIONS >> /tmp/openssl-1.0.0a-armv7.log
 perl -i -pe 's|static volatile sig_atomic_t intr_signal|static volatile int intr_signal|' crypto/ui/ui_openssl.c
 perl -i -pe 's|^CC= gcc|CC= /Developer/Platforms/iPhoneOS.platform/Developer/usr/bin/gcc -arch armv7|g' Makefile
-perl -i -pe 's|^CFLAG= (.*)|CFLAG= -isysroot /Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS4.2.sdk $1|g' Makefile
+perl -i -pe 's|^CFLAG= (.*)|CFLAG= -isysroot /Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS4.3.sdk $1|g' Makefile
 make &> /tmp/openssl-1.0.0a-armv7.log
 make install &> /tmp/openssl-1.0.0a-armv7.log
 popd
@@ -51,7 +51,7 @@ cd openssl-1.0.0a
 ./configure BSD-generic32 --openssldir=/tmp/openssl-1.0.0a-i386 $OPENSSL_CONFIGURE_OPTIONS >> /tmp/openssl-1.0.0a-i386.log
 perl -i -pe 's|static volatile sig_atomic_t intr_signal|static volatile int intr_signal|' crypto/ui/ui_openssl.c
 perl -i -pe 's|^CC= gcc|CC= /Developer/Platforms/iPhoneSimulator.platform/Developer/usr/bin/gcc -arch i386|g' Makefile
-perl -i -pe 's|^CFLAG= (.*)|CFLAG= -isysroot /Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator4.2.sdk $1|g' Makefile
+perl -i -pe 's|^CFLAG= (.*)|CFLAG= -isysroot /Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator4.3.sdk $1|g' Makefile
 make &> /tmp/openssl-1.0.0a-i386.log
 make install &> /tmp/openssl-1.0.0a-i386.log
 popd
@@ -63,16 +63,22 @@ mkdir include
 cp -r /tmp/openssl-1.0.0a-i386/include/openssl include/
 
 mkdir lib
+
 lipo \
 	/tmp/openssl-1.0.0a-armv6/lib/libcrypto.a \
 	/tmp/openssl-1.0.0a-armv7/lib/libcrypto.a \
-	/tmp/openssl-1.0.0a-i386/lib/libcrypto.a \
-	-create -output lib/libcrypto.a
+	-create -output lib/libcrypto-device.a
 lipo \
 	/tmp/openssl-1.0.0a-armv6/lib/libssl.a \
 	/tmp/openssl-1.0.0a-armv7/lib/libssl.a \
+	-create -output lib/libssl-device.a
+
+lipo \
+	/tmp/openssl-1.0.0a-i386/lib/libcrypto.a \
+	-create -output lib/libcrypto-simulator.a
+lipo \
 	/tmp/openssl-1.0.0a-i386/lib/libssl.a \
-	-create -output lib/libssl.a
+	-create -output lib/libssl-simulator.a
 
 #rm -rf /tmp/openssl-1.0.0a-*
 #rm -rf /tmp/openssl-1.0.0a-*.log
